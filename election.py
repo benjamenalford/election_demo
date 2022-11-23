e0c41c907368b4bd201f0298199a40081e35df6b
Benjamen Alford <benjamen.alford@gmail.com>
2022-11-22 21:36:46 -0600

    starting to add candidate vote count

--- election.py
+++ election.py
@@ -6,6 +6,10 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+total_votes =0
+candidates = []
+candidate_votes = {}
+# {"name":"","votes":0}
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -13,3 +17,15 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
+    for row in file_reader:
+        total_votes += 1
+        candidate = row[2]
+        #check if in candidate list
+        if candidate not in candidates:
+            candidates.append(candidate)
+        #add to candidate votes
+
+
+
+print(f'Total Votes: {total_votes}')
+print(f'We had {len(candidates)} candidates')
